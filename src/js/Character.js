4d9296c23fc06048d3cd431eb224ba113c4cba7f
Test <kia283@ya.ru>
2023-12-03 21:21:54 +1000

    Change Character

--- src/js/Character.js
+++ src/js/Character.js
@@ -4,8 +4,8 @@ export default class Character {
         this.type = type;
         this.health = 100;
         this.level = 1;
-        this.attack = undefined;
-        this.defence = undefined;
+        this.attack = null;
+        this.defence = null;
         const typeHeroes = [
             'Bowman', 
             'Swordsman', 
